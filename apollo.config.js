d4f3fb68e67091f741e49c672ab8b7e84b7b6587
HKanwal <hsk532991@gmail.com>
2022-07-05 14:48:53 -0400

    Create apollo.config.js

---
+++ apollo.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    client: {
+        service: {
+            name: "api",
+            url: "http://api.spacex.land/graphql" // TODO: change this to our endpoint
+        }
+    }
+};
