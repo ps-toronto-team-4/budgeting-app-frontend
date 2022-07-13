f481fb052059cbe6cd902467b8e473ac83335913
HKanwal <hsk532991@gmail.com>
2022-07-13 14:27:27 -0400

    Created basic welcome screen.
    Displays welcome title and has buttons to navigate to login or register screens.

--- screens/WelcomeScreen.tsx
+++ screens/WelcomeScreen.tsx
@@ -1,13 +1,24 @@
-import { StyleSheet } from 'react-native';
+import { GestureResponderEvent, StyleSheet } from 'react-native';
 
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import Button from '../components/Button';
 
 export default function WelcomeScreen({ navigation }: RootTabScreenProps<'Welcome'>) {
+  function onPressSignIn(_: GestureResponderEvent) {
+    navigation.navigate('SignIn');
+  }
+
+  function onPressSignUp(_: GestureResponderEvent) {
+    navigation.navigate('SignUp');
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Welcome to AppName!</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.title}>Welcome to</Text>
+      <Text style={styles.title}>[AppName]!</Text>
+      <Button text="Sign in" onPress={onPressSignIn}></Button>
+      <Button text="Create an account" onPress={onPressSignUp}></Button>
     </View>
   );
 }
@@ -17,9 +28,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+
   },
   title: {
-    fontSize: 36,
+    fontSize: 30,
     fontWeight: 'bold',
     fontFamily: 'Gill Sans MT'
   },
@@ -27,5 +39,5 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
-  },
+  }
 });
