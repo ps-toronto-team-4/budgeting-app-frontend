e303f660a872553841f729418a9415f5d13b78e5
gabe <51098458+gabegaghi@users.noreply.github.com>
2022-07-13 16:56:58 -0400

    Finished up the Forgot password modal

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -44,7 +44,7 @@ function RootNavigator() {
       <Stack.Screen name="SignIn" component={SignInScreen} options={{ headerShown: false }} />
       <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ headerShown: false, title: 'Oops!' }} />
       <Stack.Group screenOptions={{ presentation: 'modal' }}>
-        <Stack.Screen name="Modal" component={ForgotPasswordScreen} />
+        <Stack.Screen name="ForgotPasswordModal" component={ForgotPasswordScreen} options={{ headerShown: false, title: 'Oops!'}}/>
       </Stack.Group>
     </Stack.Navigator>
   );
