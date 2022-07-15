5d0706e57dc3aee2237c6100a28fc2d36cc7d991
HKanwal <hsk532991@gmail.com>
2022-07-15 14:42:38 -0400

    Simple changes.

--- screens/ExpenseDetailsScreen.tsx
+++ screens/ExpenseDetailsScreen.tsx
@@ -1,15 +1,35 @@
 import { useQuery } from "@apollo/client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Category, GetExpensesDocument, GetExpensesQuery } from "../components/generated";
 import { ColorValue } from "react-native"
 
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { RootStackScreenProps } from "../types";
 
-export default function ExpenseDetailsScreen() {
+export default function ExpenseDetailsScreen({ navigation }: RootStackScreenProps<'ExpenseDetails'>) {
+    const [passwordHash, setpasswordHash] = React.useState("");
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    const getData = async () => {
+        try {
+            const value = await AsyncStorage.getItem('passwordHash')
+            if (value != null) {
+                setpasswordHash(value);
+            }
+        } catch (e) {
+            setpasswordHash('undefined');
+        }
+    }
+
     return (
         <View>
             <Text>Hello from ExpenseDetailsScreen!</Text>
+            <Text>The locally stored password hash is: {passwordHash}</Text>
         </View>
     );
 }
