6d87dddd66f091a2a44e83f4c8fb6a9e236f1c6a
Harkeerat Kanwal <hsk532991@gmail.com>
2022-07-16 18:23:06 -0400

    P13 Created adaptive input component.
    It will adjust width based on input length.

---
+++ components/AdaptiveNumInput.tsx
@@ -0,0 +1,44 @@
+import { TextInput, TextInputProps, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { View } from './Themed';
+import {
+    useFonts,
+    B612Mono_400Regular,
+    B612Mono_400Regular_Italic,
+    B612Mono_700Bold,
+    B612Mono_700Bold_Italic
+} from '@expo-google-fonts/b612-mono';
+
+type AdaptiveNumInputProps = TextInputProps & {
+    /**
+     * For default fontSize, charWidth = 9.5 is best. If initializing with custom fontSize
+     * style, you gotta figure out the right charWidth yourself. Good luck.
+     */
+    charWidth: number
+};
+
+export default function AdaptiveNumInput(props: AdaptiveNumInputProps) {
+    const { style, ...otherProps } = props;
+    const [ value, setValue ] = useState('0.00');
+    const [ fontLoaded ] = useFonts({B612Mono_700Bold});
+
+    function handleChange(text: string) {
+        setValue(text);
+    }
+
+    if (!fontLoaded) {
+        return <View></View>
+    }
+
+    return (
+        <>
+            <TextInput keyboardType='numeric' value={value} style={[style, styles.input, {width: value.length * props.charWidth + props.charWidth}]} {...otherProps} onChangeText={handleChange}></TextInput>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    input: {
+        fontFamily: 'B612Mono_700Bold',
+    }
+});
