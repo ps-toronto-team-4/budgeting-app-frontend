3bc216e9ba41449af443f0788006a063aa7bfea4
HKanwal <hsk532991@gmail.com>
2022-07-20 19:53:31 -0400

    Fixed requests sent with useQuery. Adjusted header style. Refactor code to slim down on the state variable bloat.

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -1,28 +1,14 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useNavigation } from "@react-navigation/native";
-import { Alert } from "react-native";
+import { useState } from "react";
 
-export interface AuthResult {
-  loading: boolean;
-  passwordHash?: String | null;
+export function useAuth(): string {
+  const [passwordHash, setPasswordHash] = useState('');
+
+  AsyncStorage.getItem('passwordHash').then((val) => {
+    setPasswordHash(val || '');
+  }).catch((e) => {
+    console.log('Error retrieving passwordHash from AsyncStorage: ' + e);
+  });
+
+  return passwordHash;
 }
-
-export function useAuth() {
-  const navigation = useNavigation();
-    const Auth: AuthResult = {loading: true};
-    try {
-      AsyncStorage.getItem('passwordHash').then((value) => {
-        Auth.passwordHash = value;
-        Auth.loading = false;
-        if (!Auth.passwordHash) {
-          Alert.alert("You must sign in first");
-          navigation.navigate("Welcome");
-        }
-      })
-    } catch(e) {
-      Auth.loading = false;
-      console.log("couldn't get hash from async storage");
-      Alert.alert("Something went wrong");
-    }
-    return Auth;
-};
