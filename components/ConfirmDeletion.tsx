bdf545ad266f2305e80d9dff601484177d11cb9d
HKanwal <hsk532991@gmail.com>
2022-07-22 15:47:09 -0400

    Created a confirm deletion component.

--- components/ConfirmDeletion.tsx
+++ components/ConfirmDeletion.tsx
@@ -1,36 +1,74 @@
-import { useState } from "react";
-import { StyleSheet, TouchableHighlight } from "react-native";
+import { StyleSheet, TouchableOpacity } from "react-native";
+import Colors from "../constants/Colors";
 import { View, Text } from "./Themed";
 
 export interface ConfirmDeletionProps {
     onConfirm: () => void;
+    onCancel: () => void;
+    visible: boolean
 }
 
-export function ConfirmDeletion({ onConfirm }: ConfirmDeletionProps) {
-    const [visible, setVisible] = useState(false);
-
-    return (
-        <View style={styles.container}>
-            <View style={styles.messageContainer}>
-                <Text>Delete this item?</Text>
+export function ConfirmDeletion({ onConfirm, onCancel, visible }: ConfirmDeletionProps) {
+    if (visible) {
+        return (
+            <View style={styles.container}>
+                <View style={styles.messageContainer}>
+                    <Text>Delete this item?</Text>
+                </View>
+                <View style={styles.optionsRow}>
+                    <TouchableOpacity onPress={onCancel} style={styles.cancelBtn}>
+                        <Text style={styles.text}>Cancel</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={onConfirm} style={styles.confirmBtn}>
+                        <Text style={styles.text}>Yes, delete</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
-            <TouchableHighlight onPress={ }>
-                <Text>Cancel</Text>
-            </TouchableHighlight>
-        </View>
-    );
+        );
+    } else {
+        return <></>
+    }
 }
+
+const height = 200;
+const width = 250;
+const borderRadius = 15;
 
 const styles = StyleSheet.create({
     container: {
-        borderRadius: 15,
+        height: height,
+        width: width,
+        borderRadius: borderRadius,
+    },
+    messageContainer: {
+        flex: 2,
         backgroundColor: 'white',
-        height: 200,
-        width: 300,
+        borderTopStartRadius: borderRadius,
+        borderTopEndRadius: borderRadius,
         justifyContent: 'center',
         alignItems: 'center',
     },
-    messageContainer: {
-
+    optionsRow: {
+        flex: 1,
+        flexDirection: 'row',
+        borderBottomStartRadius: borderRadius,
+        borderBottomEndRadius: borderRadius,
+    },
+    cancelBtn: {
+        flex: 1,
+        backgroundColor: Colors.light.btnBackground,
+        borderBottomStartRadius: borderRadius,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        color: 'white',
+    },
+    confirmBtn: {
+        flex: 1,
+        backgroundColor: 'rgb(255,0,0)',
+        borderBottomEndRadius: borderRadius,
+        alignItems: 'center',
+        justifyContent: 'center',
     },
 });
--- screens/BudgetScreen.tsx
+++ screens/BudgetScreen.tsx
@@ -1,38 +1,21 @@
 import { useQuery } from "@apollo/client";
 import { useState, useEffect } from "react";
-import { Category, GetExpensesDocument, GetExpensesQuery } from "../components/generated";
-import { ColorValue } from "react-native"
 
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { RootTabScreenProps } from "../types";
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useAuth } from "../hooks/useAuth";
-import { Row } from "../components/Row";
-import { TouchableRow } from "../components/TouchableRow";
-import { ConfirmDeletion } from "../components/ConfirmDeletion";
 
 export default function BudgetScreen({ navigation }: RootTabScreenProps<'Budget'>) {
     const passwordHash = useAuth();
 
     return (
-        <View style={styles.container}>
-            <View style={styles.screen}>
-                <Text>Hello from BudgetScreen!</Text>
-                <Text>The locally stored password hash is: {passwordHash}</Text>
-            </View>
-            <ConfirmDeletion onConfirm={() => console.log('deletion confirmed')} />
+        <View>
+            <Text>Hello from BudgetScreen!</Text>
+            <Text>The locally stored password hash is: {passwordHash}</Text>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    screen: {
-        backgroundColor: 'white',
-    },
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'space-around',
-    },
 });
