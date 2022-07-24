3602f3dc884fc4445a34661443521c87a9e4482f
Harkeerat Kanwal <hsk532991@gmail.com>
2022-07-24 17:33:49 -0400

    Small bugfix

--- components/InputRow.tsx
+++ components/InputRow.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ReactElement, useRef, useState } from "react";
 import { StyleSheet, TextInput } from "react-native";
 import { Row, RowProps } from "./Row";
 import { View, Text } from "./Themed";
@@ -16,42 +16,40 @@ export type InputRowProps = Omit<RowProps, 'children'> & {
 /**
  * Not tested for the case where the `wrap` prop changes over component lifecycle. So keep `wrap` static to be safe.
  */
-export function InputRow({ label, placeholder, value, onChangeText, disabled, wrap, error, ...otherProps }: InputRowProps) {
+export function InputRow({ label, placeholder, value, onChangeText, wrap, error, disabled, ...otherProps }: InputRowProps) {
     const [ inputHeight, setInputHeight ] = useState(20);
 
     return (
         <Row {...otherProps}>
-            <View>
+            <View style={styles.container}>
                 <View style={styles.labelAndInput}>
                     <Text style={styles.label}>
                         {label}
                     </Text>
-                    <View style={styles.inputAndError}>
-                        <TextInput
-                            style={[styles.input, { height: inputHeight }]}
-                            placeholder={placeholder}
-                            value={value}
-                            onChangeText={onChangeText}
-                            editable={disabled !== true}
-                            multiline={wrap}
-                            scrollEnabled={false}
-                            textAlignVertical="top"
-                            onContentSizeChange={(e) => setInputHeight(e.nativeEvent.contentSize.height)} />
-                    </View>
+                    <TextInput
+                        style={[styles.input, { height: inputHeight }]}
+                        placeholder={placeholder}
+                        value={value}
+                        onChangeText={onChangeText}
+                        editable={disabled !== true}
+                        multiline={wrap}
+                        scrollEnabled={false}
+                        textAlignVertical="top"
+                        onContentSizeChange={(e) => setInputHeight(e.nativeEvent.contentSize.height)} />
                 </View>
-                        {
-                            error &&
-                                <Text style={styles.error}>
-                                    {error}
-                                </Text>
-                        }
+                {
+                    error &&
+                        <Text style={styles.error}>
+                            {error}
+                        </Text>
+                }
             </View>
         </Row>
     );
 }
 
 const styles = StyleSheet.create({
-    inputAndError: {
+    container: {
         backgroundColor: 'rgba(0,0,0,0)',
     },
     labelAndInput: {
