86f898acab6598c91278f3026ee524bab437ab39
NasihNazeem <73755710+NasihNazeem@users.noreply.github.com>
2022-07-26 17:07:54 -0400

    Pie Graph is functional, animating later and implementing arguments for ByCategory later

--- components/GraphDisplays/byCategory.tsx
+++ components/GraphDisplays/byCategory.tsx
@@ -1,12 +1,147 @@
-import { View } from "react-native";
+import { useQuery } from "@apollo/client";
+import React from "react";
+import { View, Text } from "react-native";
+import Svg from "react-native-svg";
+import { VictoryChart, VictoryLabel, VictoryLegend, VictoryPie } from "victory-native";
+import { useAuth } from "../../hooks/useAuth";
+import { useRefresh } from "../../hooks/useRefresh";
+import { Category, GetCategoriesDocument, GetCategoriesQuery, GetMonthBreakdownDocument, GetMonthBreakdownQuery, MonthBreakdown, MonthBreakdownCategory } from "../generated";
 
-
-const Render = () => {
-
-
-    return (<View>
-        Render
-    </View>)
+type byCategoryProps = {
+    CategoryData: MonthBreakdownCategory[];
+    Month: string;
+    Year: number;
 }
 
-export default Render;
+export default function byCategory({ CategoryData, Month, Year }: byCategoryProps) {
+
+    const passwordHash = useAuth();
+    const [month, setMonth] = React.useState("JULY");
+    const [year, setYear] = React.useState("2022");
+    const listOfAmountAndCategories: MonthBreakdownCategory[] = [];
+    const listofCategories: Category[] = [];
+    const [listofMonthNames, setListOfMonthNames] = React.useState([]);
+
+    const { loading: monthlyBreakdownLoading, data: monthlyBreakdownData, refetch: monthlyBreakdownRefetch } = useQuery<GetMonthBreakdownQuery>(GetMonthBreakdownDocument,
+        { variables: { passwordHash: passwordHash, month: month, year: year } }
+    )
+
+    const { loading: categoriesLoading, data: categoriesData, refetch: categoriesRefetch } = useQuery<GetCategoriesQuery>(GetCategoriesDocument,
+        {
+            variables: { passwordHash: passwordHash }
+        })
+
+    useRefresh(() => {
+        monthlyBreakdownRefetch();
+    }, [passwordHash]);
+
+    function getMonthNames() {
+        if (categoriesData?.categories.__typename === "CategoriesSuccess") {
+
+        }
+    }
+
+    // Function to get Month Names in an array format for categories attribute
+
+    // Constant to return data dynamically so that I do not have to manually insert each data point 
+    // (returns {x: Name, y: Value, fill: "#colorHex", label: "$" + Value})
+
+    return (
+
+        <View style={{ justifyContent: "center", alignItems: "center" }}>
+            <View>
+                <View style={{ position: "absolute", height: '100%', width: '100%', justifyContent: "center", alignItems: "center" }}>
+                    <Text>{year}</Text>
+                    <Text>{month}</Text>
+                </View>
+                <VictoryPie
+                    padAngle={3}
+                    innerRadius={100}
+                    data={
+                        CategoryData.map((data) => {
+                            if (data.category === null) {
+                                return {
+                                    amountSpent: data.amountSpent,
+                                    category: {
+                                        name: "Uncategorized",
+                                        colourHex: "#757575",
+                                    }
+                                }
+                            } else {
+                                return data
+                            }
+                        })
+
+                    }
+                    x={"category.name"}
+                    y={"amountSpent"}
+                    colorScale={CategoryData.map((data) => data.category ? "#" + data.category.colourHex : "gray")}
+                    width={600}
+                    events={
+                        [{
+                            target: "data",
+                            eventHandlers: {
+                                onClick: () => {
+                                    return (
+                                        {
+                                            target: "labels",
+                                            mutation: (props) => {
+                                                console.log(props);
+                                                return props.text === "$" + props.datum.amountSpent ? null : { text: "$" + props.datum.amountSpent.toFixed(2) };
+                                            }
+                                        }
+                                    );
+                                }
+                            }
+                        }]}
+                />
+
+            </View>
+            <VictoryLegend
+                centerTitle={true}
+                orientation="horizontal"
+                colorScale={CategoryData.map((data) => data.category ? "#" + data.category.colourHex : "gray")}
+                data={
+                    CategoryData.map((data) => {
+                        if (data.category === null) {
+                            return {
+                                name: "Uncategorized",
+                                symbol: {
+                                    fill: "#757575",
+                                }
+                            }
+                        }
+                        else {
+                            return {
+                                name: data.category?.name,
+                                symbol: {
+                                    fill: "#" + data.category?.colourHex
+                                }
+                            }
+                        }
+                    })
+
+
+                }
+                itemsPerRow={3}
+                gutter={50}
+            />
+        </View>
+
+
+
+        // <View>
+        //     <VictoryPie
+        //         padAngle={({ datum }) => datum.y}
+        //         innerRadius={100}
+        //         data={
+        //             [
+
+        //             ]
+        //         }
+
+        //     />
+        // </View>
+    );
+
+}
--- screens/ReportsScreen.tsx
+++ screens/ReportsScreen.tsx
@@ -7,7 +7,8 @@ import { Screen } from "../components/Screen";
 import Styles from '../constants/Styles';
 import { VictoryChart, VictoryLegend, VictoryPie } from 'victory-native';
 import { useQuery } from '@apollo/client';
-import { GetCategoriesDocument, GetCategoriesQuery } from '../components/generated';
+import { GetCategoriesDocument, GetCategoriesQuery, MonthBreakdownCategory } from '../components/generated';
+import ByCategory from '../components/GraphDisplays/ByCategory';
 
 export default function ReportsScreen({ navigation }: RootTabScreenProps<'Reports'>) {
     const passwordHash = useAuth();
@@ -20,37 +21,77 @@ export default function ReportsScreen({ navigation }: RootTabScreenProps<'Report
 
     return (
         <Screen>
-            <View style={Styles.center}>
-                <VictoryPie
-                    padAngle={({ datum }) => datum.y}
-                    innerRadius={100}
-                    categories={{ x: ["Test1", "Test2"] }}
-                    data={[
-                        { x: "Test1", y: 5, label: "$" + 5, fill: "purple" },
-                        { x: "Test2", y: 6, label: "$" + 6, fill: "black" },
-
-                    ]}
-                    style={{
-                        data:
-                        {
-                            fill:
-                                ({ datum }) => datum.fill,
-                        }
-                    }}
-                    width={500}
-                />
-            </View>
-            <View style={Styles.center}>
-                <VictoryLegend
-                    title="Legend"
-                    centerTitle
-                    orientation='horizontal'
-                    gutter={50}
-                    data={[
-                        { name: "Planned" }, { name: "Actual" }
-                    ]}
-                />
-            </View>
+            <Text> This is the Reports Screen! </Text>
+            <ByCategory CategoryData={[
+                {
+                    "category": null,
+                    "amountSpent": 53.099999999999994
+                },
+                {
+                    "category": {
+                        "name": "Loans",
+                        "colourHex": "97E2D5",
+                        "id": 4
+                    },
+                    "amountSpent": 460.67
+                },
+                {
+                    "category": {
+                        "name": "Cell Phone Bill",
+                        "colourHex": "EF7300",
+                        "id": 8
+                    },
+                    "amountSpent": 434.2099999999999
+                },
+                {
+                    "category": {
+                        "name": "Electronics",
+                        "colourHex": "C1A6FA",
+                        "id": 6
+                    },
+                    "amountSpent": 147.42000000000002
+                },
+                {
+                    "category": {
+                        "name": "Car Insurance",
+                        "colourHex": "FFC803",
+                        "id": 7
+                    },
+                    "amountSpent": 132.38
+                },
+                {
+                    "category": {
+                        "name": "Dining Out",
+                        "colourHex": "FEC600",
+                        "id": 3
+                    },
+                    "amountSpent": 43.12
+                },
+                {
+                    "category": {
+                        "name": "Transportation",
+                        "colourHex": "009418",
+                        "id": 5
+                    },
+                    "amountSpent": 266.38
+                },
+                {
+                    "category": {
+                        "name": "Rent",
+                        "colourHex": "0018F2",
+                        "id": 2
+                    },
+                    "amountSpent": 339.97
+                },
+                {
+                    "category": {
+                        "name": "Groceries",
+                        "colourHex": "5BEF007D",
+                        "id": 1
+                    },
+                    "amountSpent": 232.66
+                }
+            ] as MonthBreakdownCategory[]} Month={'JANUARY'} Year={2022}></ByCategory>
         </Screen>
     );
 }
