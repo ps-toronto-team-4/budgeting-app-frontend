a9ec956cfad3b2489aca0c76c8b126da1d0bbe7e
Harkeerat Kanwal <hsk532991@gmail.com>
2022-07-29 13:39:52 -0400

    Created settings screen.

---
+++ components/profile/SettingsBar.tsx
@@ -0,0 +1,57 @@
+import { useMemo } from "react";
+import { StyleSheet, TouchableHighlight, View, Text } from "react-native";
+import { Entypo } from '@expo/vector-icons';
+
+export interface SettingsBarProps {
+    title: string,
+    onPress: () => void;
+    subtitle?: string,
+    topBorder?: boolean;
+    bottomBorder?: boolean;
+}
+
+export function SettingsBar({ title, onPress, subtitle, topBorder, bottomBorder }: SettingsBarProps) {
+    const dynamicStyles = useMemo(() => {
+        return StyleSheet.create({
+            container: {
+                backgroundColor: '#f5f5f5',
+                borderTopWidth: topBorder ? 1 : 0,
+                borderBottomWidth: bottomBorder ? 1 : 0,
+                borderTopColor: 'rgba(0,0,0,0.2)',
+                borderBottomColor: 'rgba(0,0,0,0.2)',
+                paddingVertical: 20,
+            }
+        });
+    }, [topBorder, bottomBorder]);
+
+    return (
+        <TouchableHighlight style={dynamicStyles.container} onPress={onPress} underlayColor="rgba(0,0,0,0.2)">
+            <View style={staticStyles.content}>
+                <View style={staticStyles.titlesContiainer}>
+                    <Text style={staticStyles.title}>{title}</Text>
+                    <Text style={staticStyles.subtitle}>{subtitle || 'no subtitle given'}</Text>
+                </View>
+                <Entypo name="chevron-right" size={24} color="black" style={staticStyles.icon} />
+            </View>
+        </TouchableHighlight>
+    );
+}
+
+const staticStyles = StyleSheet.create({
+    content: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    titlesContiainer: {
+        width: 270,
+        marginRight: 20,
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        paddingBottom: 5,
+    },
+    subtitle: {},
+    icon: {},
+});
--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, SafeAreaView } from 'react-native';
+import React, { useEffect, useMemo } from 'react';
+import { View, Text, TouchableHighlight, SafeAreaView, StyleSheet, ScrollView } from 'react-native';
 import { RootTabScreenProps } from "../types";
 import Button from "../components/Button";
 import { useAuth } from "../hooks/useAuth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useUnauthRedirect } from "../hooks/useUnauthRedirect";
 import { Screen } from "../components/Screen";
-import { MaterialIcons } from "@expo/vector-icons";
+import { MaterialIcons, FontAwesome5 } from "@expo/vector-icons";
 import Styles from '../constants/Styles';
+import { useQuery } from '@apollo/client';
+import { GetUserDocument, GetUserQuery, GetUserQueryVariables } from '../components/generated';
+import { useRefresh } from '../hooks/useRefresh';
+import { SettingsBar } from '../components/profile/SettingsBar';
+
+function formatPhoneNumber(phoneNumber: string): string {
+    return `${phoneNumber.slice(0, 3)}-${phoneNumber.slice(3, 6)}-${phoneNumber.slice(6)}`;
+}
 
 export default function ProfileScreen({ navigation }: RootTabScreenProps<'Profile'>) {
-    
     const passwordHash = useAuth();
+    const { data, refetch } = useQuery<GetUserQuery, GetUserQueryVariables>(GetUserDocument, {
+        variables: { passwordHash }
+    });
+    useRefresh(refetch, [passwordHash]);
+    useUnauthRedirect();
 
     const logout = () => {
         AsyncStorage.multiRemove(['passwordHash', 'New Category']).then(() => {
@@ -19,25 +31,121 @@ export default function ProfileScreen({ navigation }: RootTabScreenProps<'Profil
         });
     };
 
-    useUnauthRedirect();
+    useEffect(() => {
+        navigation.setOptions({
+            headerRight: () => (
+                <Text style={styles.logoutBtn}>Log Out</Text>
+            )
+        });
+    }, []);
 
-    return (
-        <Screen>
-            <Text>Hello from ProfileScreen!</Text>
-            <Text>The locally stored password hash is: {passwordHash}</Text>
-            <TouchableOpacity accessibilityLabel="Link to Category Settings" onPress={() => navigation.navigate('CategorySettings')}>
-                <View style={Styles.list}>
-                    <Text style={{ fontSize: 18, marginHorizontal: 5 }}>Categories</Text>
-                    <MaterialIcons name="navigate-next" size={24} color="black" />
+    if (!data) {
+        return <Screen></Screen>
+    } else if (data?.user.__typename !== 'User') {
+        return <Text>Error fetching user data.</Text>;
+    } else {
+        const user = data.user;
+
+        return (
+            <ScrollView style={styles.screen}>
+                <FontAwesome5 name="user-circle" size={80} color="black" style={styles.userIcon} />
+                <View style={styles.userDetailsContainer}>
+                    <View style={styles.userDetailsRow}>
+                        <Text style={styles.userDetailsLabel}>First name</Text>
+                        <Text style={styles.userDetailsValue}>{user.firstName}</Text>
+                    </View>
+                    <View style={styles.userDetailsRow}>
+                        <Text style={styles.userDetailsLabel}>Last name</Text>
+                        <Text style={styles.userDetailsValue}>{user.lastName}</Text>
+                    </View>
+                    <View style={styles.userDetailsRow}>
+                        <Text style={styles.userDetailsLabel}>Email</Text>
+                        <Text style={styles.userDetailsValue}>{data.user.email}</Text>
+                    </View>
+                    <View style={styles.userDetailsRow}>
+                        <Text style={styles.userDetailsLabel}>Phone</Text>
+                        <Text style={styles.userDetailsValue}>{user.phoneNumber ? formatPhoneNumber(user.phoneNumber) : 'None'}</Text>
+                    </View>
                 </View>
-            </TouchableOpacity>
-            <TouchableOpacity accessibilityLabel="Link to Merchant Settings" onPress={() => navigation.navigate('MerchantSettings')}>
-                <View style={Styles.list}>
-                    <Text style={{ fontSize: 18, marginHorizontal: 5 }}>Merchants</Text>
-                    <MaterialIcons name="navigate-next" size={24} color="black" />
+                <View style={styles.settingsList}>
+                    <SettingsBar
+                        title='Categories'
+                        subtitle='Modify your list of existing categories'
+                        onPress={() => navigation.navigate('CategorySettings')}
+                        topBorder />
+                    <SettingsBar
+                        title='Merchant'
+                        subtitle='Modify your list of existing merchants'
+                        onPress={() => navigation.navigate('MerchantSettings')}
+                        topBorder />
+                    <SettingsBar
+                        title='Security'
+                        subtitle='View or change your password'
+                        onPress={() => alert('Security screen is not yet implemented.')}
+                        topBorder />
+                    <SettingsBar
+                        title='Notifications'
+                        subtitle='Choose how often you want to recieve notifications'
+                        onPress={() => alert('Notifications screen is not yet implemented.')}
+                        topBorder />
+                    <SettingsBar
+                        title='Help'
+                        subtitle='Contact our Helpdesk'
+                        onPress={() => alert('Help screen is not yet implemented.')}
+                        topBorder />
+                    <SettingsBar
+                        title='Privacy Policy'
+                        subtitle='Learn more about this app'
+                        onPress={() => alert('Security screen is not yet implemented.')}
+                        topBorder
+                        bottomBorder />
                 </View>
-            </TouchableOpacity>
-            <Button text="Logout" onPress={logout} accessibilityLabel="Logout Button" />
-        </Screen>
-    );
+            </ScrollView>
+        );
+    }
 }
+
+const userDetailsFontSize = 17;
+const userDetailsVerticalSpacing = 5;
+const userDetailsHorizontalSpacing = 20;
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        backgroundColor: 'white',
+    },
+    logoutBtn: {
+        color: 'red',
+        paddingRight: 30,
+        fontWeight: 'bold',
+        fontSize: 17,
+    },
+    userIcon: {
+        alignSelf: 'center',
+        marginTop: 10,
+        marginBottom: 20,
+    },
+    userDetailsContainer: {
+        alignItems: 'center',
+    },
+    userDetailsRow: {
+        flexDirection: 'row',
+        paddingVertical: userDetailsVerticalSpacing,
+    },
+    userDetailsLabel: {
+        width: 100,
+        textAlign: 'right',
+        fontSize: userDetailsFontSize,
+    },
+    userDetailsValue: {
+        fontWeight: 'bold',
+        width: 250,
+        paddingLeft: userDetailsHorizontalSpacing,
+        paddingRight: 20,
+        fontSize: userDetailsFontSize,
+    },
+    settingsList: {
+        marginTop: 25,
+        marginBottom: 20,
+    },
+});
