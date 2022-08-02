313583d919408240bce6b5aaf3c59d8f7d6796b4
Calvin Mozola <calvin@stuntfactory.com>
2022-08-02 10:43:23 -0400

    Stash:

--- screens/ReportScreens/ExpandBarCat.tsx
+++ screens/ReportScreens/ExpandBarCat.tsx
@@ -1,15 +1,34 @@
 import { Text, View, StyleSheet } from "react-native";
 import { RootStackScreenProps } from "../../types";
 import { useState } from "react";
+import { useAuth } from "../../hooks/useAuth";
+import { useLazyQuery } from '@apollo/client';
+import { GetMonthBreakdownDocument, GetMonthBreakdownQuery, GetMonthBreakdownQueryVariables, MonthType } from "../../components/generated";
 
 
 
 export default function ExpandExpense({ navigation, route }: RootStackScreenProps<'ExpandExpenses'>) {
 
+    const passwordHash = useAuth({
+        onRetrieved: (passwordHash) => {
+            getMonthlyBreakdown()
+        },
+        redirect: 'ifUnauthorized',
+    });
+
     const [month, setMonth] = useState(route.params.month)
     const [year, setYear] = useState(route.params.year)
 
+    const [getMonthlyBreakdown, { loading: monthlyBreakdownLoading, data: monthlyBreakdownData, refetch: monthlyBreakdownRefetch }] = useLazyQuery<GetMonthBreakdownQuery, GetMonthBreakdownQueryVariables>(GetMonthBreakdownDocument,
+        { variables: { passwordHash, month: month as MonthType, year } });
+
     return <View style={staticStyles.screen}>
+        {/* <MonthlyVsBudgetedCategory
+                        data={monthlyBreakdownData?.monthBreakdown.__typename === "MonthBreakdown" ? monthlyBreakdownData.monthBreakdown.byCategory : []}
+                        budgetReferenceData={budgetsData?.budgets.__typename === 'BudgetsSuccess' ?
+                            budgetsData.budgets.budgets.find(ele => {
+                                return ele.month == month && ele.year == year
+                            }) as Budget : undefined} /> */}
         <View>
 
             <Text>
