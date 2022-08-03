f658ca669a6bef0141aaf0b8272dae6bdd8368d8
NasihNazeem <73755710+NasihNazeem@users.noreply.github.com>
2022-08-03 13:22:05 -0400

    Function for Expense Graph Insights work

--- screens/ReportScreens/ExpandExpense.tsx
+++ screens/ReportScreens/ExpandExpense.tsx
@@ -30,6 +30,8 @@ export default function ExpandExpense({ navigation, route }: RootStackScreenProp
     });
     const [month, setMonth] = useState(route.params.month);
     const [year, setYear] = useState(route.params.year);
+    const [percent, setPercent] = useState<string>();
+    const [moreOrLess, setMoreOrLess] = useState(false);
     const [topMerchant, setTopMerchant] = useState<string | undefined>("");
     const [getMonthTotals, { loading: monthTotalsLoading, data: monthTotalsData, refetch: monthTotalsRefetch }] = useLazyQuery<GetMonthTotalsQuery>(GetMonthTotalsDocument,
         { variables: { passwordHash } });
@@ -44,30 +46,41 @@ export default function ExpandExpense({ navigation, route }: RootStackScreenProp
         monthTotalsRefetch();
     })
 
-    useEffect(() => {
-        console.log(monthBreakdownData?.monthBreakdown.__typename);
+
+    const RetreiveTopMerchant = () => {
         if (!monthBreakdownLoading && monthBreakdownData?.monthBreakdown.__typename === "MonthBreakdown") {
             setTopMerchant(monthBreakdownData.monthBreakdown.topMerchant?.merchant?.name);
         } else {
             setTopMerchant("Undefined");
         }
-    })
-    // const RetreiveTopMerchant = () => {
-    //     console.log(monthBreakdownData?.monthBreakdown.__typename);
-    //     if (!monthBreakdownLoading && monthBreakdownData?.monthBreakdown.__typename === "MonthBreakdown") {
-    //         setTopMerchant(monthBreakdownData.monthBreakdown.topMerchant?.merchant?.name);
-    //     } else {
-    //         setTopMerchant("Undefined");
-    //     }
 
-    //     return (
-    //         <Text>{topMerchant}</Text>
-    //     );
-    // }
+        return (
+            <Text style={{ fontWeight: 'bold' }}>{topMerchant}</Text>
+        );
+    }
+
+    const PercentCalculation = () => {
+        if (monthTotalsData?.monthsTotals.__typename === "MonthsTotals" && monthTotalsData.monthsTotals.byMonth.length >= 2) {
+            let previousMonthIndex = monthTotalsData.monthsTotals.byMonth.findIndex(e => e.month === month) - 1;
+            let currentMonthSpent = monthTotalsData.monthsTotals.byMonth[previousMonthIndex + 1].amountSpent;
+            let previousMonthSpent = monthTotalsData.monthsTotals.byMonth[previousMonthIndex].amountSpent;
+            let delta = (currentMonthSpent - previousMonthSpent) / (previousMonthSpent);
+
+            if (delta > 0) {
+                setMoreOrLess(true)
+            }
+
+            setPercent(Math.abs((100 * (delta))).toFixed(1))
+            console.log();
+        }
+
+
+        return (
+            <Text style={{ fontWeight: 'bold' }}>{percent}% {moreOrLess ? "more" : "less"}</Text>
+        );
+    }
 
     return <View style={staticStyles.screen}>
-
-        {/* <TopBar month={month} year={year} setMonth={setMonth} setYear={setYear} /> */}
 
         <View>
             <Text style={{ flex: 1, textAlign: 'center', fontWeight: 'bold', marginVertical: 20 }}>
@@ -82,11 +95,26 @@ export default function ExpandExpense({ navigation, route }: RootStackScreenProp
                 yearSelector={year} />
         </View>
 
-        <View style={{ justifyContent: "center" }}>
-            {/* <RetreiveTopMerchant></RetreiveTopMerchant> */}
-            <Text style={{ flex: 1, textAlign: 'center', fontWeight: 'bold', marginVertical: 20 }}>
-                {topMerchant} is your top spending Merchant
-            </Text>
+        <View style={{ justifyContent: "center", alignContent: 'center' }}>
+            <View style={{ flexDirection: 'row', justifyContent: "center" }}>
+                <RetreiveTopMerchant />
+                <Text style={{ paddingLeft: 5, flexWrap: 'wrap', width: 200 }}>is your top spending Merchant</Text>
+            </View>
+            {/* <View style={{ flexDirection: 'row', justifyContent: "center" }}>
+                <Text style={{ fontWeight: 'bold', paddingLeft: 5 }}>Merchant</Text>
+            </View> */}
+        </View>
+
+        <View style={{ justifyContent: "center", alignContent: 'center', paddingTop: 20 }}>
+            <View style={{ flexDirection: 'row', justifyContent: "center" }}>
+                <Text>You spent </Text>
+                <PercentCalculation />
+            </View>
+
+        </View>
+
+        <View style={{ flexDirection: 'row', justifyContent: "center" }}>
+            <Text> this month than the previous month</Text>
         </View>
     </View >
 }
