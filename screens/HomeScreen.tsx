286a9a7da69b7a3a7543dce5123ef26f7e7500d6
Harkeerat Kanwal <hsk532991@gmail.com>
2022-08-08 22:07:08 -0400

    Fix potential divide by 0.

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -98,8 +98,11 @@ export default function HomeScreen({ navigation }: RootTabScreenProps<'Home'>) {
         let yearAvg = 0;
         if (avgData?.monthsTotals.__typename === 'MonthsTotals' && avgData.monthsTotals.averageSpent) {
             const applicableMonthData = avgData.monthsTotals.byMonth.filter(x => x.year === year).map(x => x.amountSpent);
-            console.log(applicableMonthData.length);
-            yearAvg = applicableMonthData.reduce((a, b) => a + b, 0) / applicableMonthData.length;
+            if (applicableMonthData.length === 0) {
+                yearAvg = 0;
+            } else {
+                yearAvg = applicableMonthData.reduce((a, b) => a + b, 0) / applicableMonthData.length;
+            }
         }
         return yearAvg;
     }, [avgData]);
